89a07f98d1ca0f3cddeb0f05d39c1f69224bed4f
Dat Lv <lvoddat95@yahoo.com>
2020-07-17 16:08:53 +0700

    first time

---
+++ header.php
@@ -0,0 +1,38 @@
+<!-- Header -->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="VNI">
+    <title>VNI</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="app-assets/images/ico//favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="app-assets/images/ico//favicon-16x16.png">
+    <link rel="manifest" href="app-assets/images/ico/site.webmanifest">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <!-- init Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- /init Font -->
+
+    <!-- core stylesheet -->
+    <link rel="stylesheet" type="text/css" href="app-assets/fonts/linearicons/linearicons.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/fonts/iconmoon/iconmoon.min.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/perfect-scrollbar.min.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css/bootstrap_custom.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css/components.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css/layout.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css/colors.css">
+    <!-- core stylesheet -->
+
+    <!-- custom stylesheet -->
+    <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/chartist.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css/style_custom.css">
+    <link rel="stylesheet" type="text/css" href="app-assets/css/responsive.css">
+    <!-- /custom stylesheet -->
+
+</head>
+<!-- /Header -->
