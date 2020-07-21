f42fdedca637e77f207fdb0a3ee9656f91ac332b
Dat Lv <lvoddat95@yahoo.com>
2020-07-21 09:33:09 +0700

    Update nhap-don.php

--- view/pages/nhap-don.php
+++ view/pages/nhap-don.php
@@ -1,7 +1,9 @@
 <?php require_once '../../config.php'; 
 echo '<pre>';
 print_r($_SERVER);
-print_r(include $_SERVER['DOCUMENT_ROOT'] . "/bhhk/view/inc/footer.php");
+var_dump(include $_SERVER['DOCUMENT_ROOT'] . "/bhhk/view/inc/footer.php");
+echo 'string';
+var_dump(include $_SERVER['DOCUMENT_ROOT'] . "/app/view/inc/footer.php");
 echo '</pre>';
 
 ?>
