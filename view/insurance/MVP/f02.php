20ce651726190347c43d84f535f23a3ada1c2a00
Dat Lv <lvoddat95@yahoo.com>
2020-10-06 10:56:14 +0700

    Update f02.php

--- view/insurance/MVP/f02.php
+++ view/insurance/MVP/f02.php
@@ -112,11 +112,11 @@
                                                                                     <label>Tên chủ xe: <span class="text-danger">*</span></label>
                                                                                     <div class="row">
                                                                                         <div class="col-md-4 col-sm-4">
-                                                                                            <input type="text" class="form-control required" placeholder="Nhập mã khách hàng...">
+                                                                                            <input type="text" class="form-control required" placeholder="Nhập mã chủ xe...">
                                                                                         </div>
                                                                                         <div class="col-md-8 col-sm-8">
                                                                                             <div class="input-group">
-                                                                                                <input type="text" class="form-control required" placeholder="Nhập tên khách hàng..." >
+                                                                                                <input type="text" class="form-control required" placeholder="Nhập tên chủ xe..." >
                                                                                                 <span class="input-group-append">
                                                                                                     <!-- <button type="button" class="btn bg-primary" data-toggle="modal" data-target="#ds_cac_khach_hang">Chọn từ danh mục <i class="fad fa-user-plus ml-1"></i></button> -->
                                                                                                     <button type="button" class="btn bg-primary" data-fancybox data-base-class="fancybox-container-xl" data-type="iframe" data-src="<?php echo HTTP_PATH; ?>view/inc/iframe/iframe_ds_khach_hang.php" href="javascript:;">Chọn từ danh mục <i class="fad fa-user-plus ml-1"></i></button>
