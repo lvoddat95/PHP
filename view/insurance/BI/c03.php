b38ae5d9eb4eeddd8b946f34c7c137e1c4ac0790
Dat Lv <lvoddat95@yahoo.com>
2020-12-10 10:54:53 +0700

    ok

--- view/insurance/BI/c03.php
+++ view/insurance/BI/c03.php
@@ -254,6 +254,10 @@
                                                                                         </thead>
                                                                                         <tbody>
                                                                                             <tr>
+                                                                                                <td>Mức trách nhiệm đồng bảo hiểm của VNI</td>
+                                                                                                <td class="text-right"><input class="form-control input-money text-right" type="text" value="0"></td>
+                                                                                            </tr>
+                                                                                            <tr>
                                                                                                 <td>Máy móc thiết bị của chủ thầu</td>
                                                                                                 <td class="text-right">0</td>
                                                                                             </tr>
