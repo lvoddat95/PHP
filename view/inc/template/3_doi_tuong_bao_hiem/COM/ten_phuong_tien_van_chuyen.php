25210621c57c7575417899e1b61c2eafb02a17c5
Dat Lv <lvoddat95@yahoo.com>
2021-02-19 16:42:58 +0700

    Update ten_phuong_tien_van_chuyen.php

--- view/inc/template/3_doi_tuong_bao_hiem/COM/ten_phuong_tien_van_chuyen.php
+++ view/inc/template/3_doi_tuong_bao_hiem/COM/ten_phuong_tien_van_chuyen.php
@@ -15,7 +15,7 @@
 		    <option>2.6 Hàng chở rời khác</option>
 		</select>
 		<div class="input-group-append">
-			<button type="button" class="btn bg-primary" data-toggle="modal" data-target="#ds_tau_thuyen">+ Thêm phương tiện</button>
+			<button type="button" class="btn bg-primary" data-fancybox data-base-class="fancybox-container-lg" data-type="iframe" data-src="<?php echo HTTP_PATH; ?>view/inc/iframe/iframe_ds_tau_thuyen.php" href="javascript:;">+ Thêm phương tiện</button>
 		</div>
 	</div>
 </div>
