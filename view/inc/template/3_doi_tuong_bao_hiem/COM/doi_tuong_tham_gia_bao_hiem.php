0481d00edb0113dbbe6f8fef1a0e29029218fae4
Dat Lv <lvoddat95@yahoo.com>
2021-04-02 10:38:27 +0700

    ok

--- view/inc/template/3_doi_tuong_bao_hiem/COM/doi_tuong_tham_gia_bao_hiem.php
+++ view/inc/template/3_doi_tuong_bao_hiem/COM/doi_tuong_tham_gia_bao_hiem.php
@@ -1,9 +1,4 @@
 <div class="form-group">
 	<label>Đối tượng tham gia bảo hiểm: <span class="text-danger">*</span></label>
-    <div class="input-group">
-        <input class="form-control" type="text" name="" value="" placeholder="Nhập đối tượng bảo hiểm...">
-        <span class="input-group-append">
-            <button type="button" class="btn bg-primary" data-fancybox data-base-class="fancybox-container-xl" data-type="iframe" data-src="<?php echo HTTP_PATH; ?>view/inc/iframe/iframe_ds_khach_hang.php" href="javascript:;">Chọn</button>
-        </span>
-    </div>
+    <input class="form-control" type="text" name="" value="" placeholder="Nhập đối tượng bảo hiểm...">
 </div>
--- view/inc/template/3_doi_tuong_bao_hiem/COM/ten_phuong_tien_van_chuyen.php
+++ view/inc/template/3_doi_tuong_bao_hiem/COM/ten_phuong_tien_van_chuyen.php
@@ -3,16 +3,18 @@
 	<div class="input-group">
 		<select class="form-control" select2>
 			<option>--- Chọn ---</option>
-		    <option>1.2 Hàng đóng contranier khác</option>
-		    <option>2.1 Hàng xá có mở rộng xác định trọng lượng qua cân tại cảng chính</option>
-		    <option>2.2 Hàng xá được BH theo (điều kiện C hoặc B hoặc VCNĐ) + thiếu hụt (bao gồm cả thiếu hụt nguyên thanh, nguyên bó, nguyên cuộn).</option>
-		    <option>2.3 Hàng rời bảo hiểm theo điều kiện “Bulk oil + thiếu hụt” có áp dụng mức khấu trừ</option>
-		    <option>4. Hàng hóa khác.</option>
-		    <option>1.1 Hàng đóng contrainer(Không bao gồm hàng nông sản)</option>
-		    <option>3. Hàng vận chuyển đường hàng không</option>
-		    <option>2.4 Hàng xá có mở rộng xác định trọng lượng qua cân tại cảng lẻ nội địa</option>
-		    <option>2.5 Hàng xá có mở rộng xác định trọng lượng qua mớn nước</option>
-		    <option>2.6 Hàng chở rời khác</option>
+		    <option id="TAU10311" name="HE YUAN    V. 2113E" value="TAU10311">HE YUAN V. 2113E</option><option id="TAU10308" name="Chở khách" value="TAU10308">Chở khách</option>
+			<option id="TAU10320" name="ADRIAN MAERSK 107E" value="TAU10320">ADRIAN MAERSK 107E</option><option id="TAU10350" name="WAN HAI 231 S328" value="TAU10350">WAN HAI 231 S328</option>
+			<option id="TAU10357" name="JL751/24" value="TAU10357">JL751/24</option><option id="TAU10369" name="VESSEL NAME: MCC NANJING Voy. No: 110V" value="TAU10369">VESSEL NAME: MCC NANJING Voy. No: 110V</option>
+			<option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option><option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option>
+			<option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option><option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option>
+			<option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option><option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option>
+			<option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option><option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option>
+			<option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option><option id="TAU10379" name="ONE IBIS V.022E" value="TAU10379">ONE IBIS V.022E</option>
+			<option id="TAU10385" name="NĐ 3348" value="TAU10385">NĐ 3348</option><option id="TAU10385" name="NĐ 3348" value="TAU10385">NĐ 3348</option><option id="TAU00001" name="Tàu victory A" value="TAU00001">Tàu victory A</option>
+			<option id="TAU10071" name="LONG DƯƠNG 22" value="TAU10071">LONG DƯƠNG 22</option><option id="TAU10076" name="HUA KAI           V. 2109E" value="TAU10076">HUA KAI V. 2109E</option>
+			<option id="TAU10080" name="MARCLIFF 21005N" value="TAU10080">MARCLIFF 21005N</option>
+
 		</select>
 		<div class="input-group-append">
 			<button type="button" class="btn bg-primary" data-fancybox data-base-class="fancybox-container-lg" data-type="iframe" data-src="<?php echo HTTP_PATH; ?>view/inc/iframe/iframe_ds_tau_thuyen.php" href="javascript:;">+ Thêm phương tiện</button>
