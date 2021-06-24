9814bb6c4691e86839738692fde561090614c97d
Dat Lv <lvoddat95@yahoo.com>
2021-06-24 16:10:42 +0700

    ss

--- view/tbh/hop-dong-tbh/them-moi-hop-dong.php
+++ view/tbh/hop-dong-tbh/them-moi-hop-dong.php
@@ -207,7 +207,7 @@
                                             </div>
 
                                             <div class="row form-group">
-                                                <label class="col-label col-label-150 col-form-label">Tình trạng áp dụng:<span class="text-danger">*</span></label>
+                                                <label class="col-label col-label-150 col-form-label">Tình trạng áp dụng:</label>
                                                 <div class="col-content col-content-xl d-inline-flex align-items-center">
                                                     <div class="button-switch mr-2">
                                                         <input type="checkbox" id="tttpv" class="switch" name="tttpv" checked="">
